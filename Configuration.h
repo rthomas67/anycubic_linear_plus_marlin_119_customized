e11dbe96c1f21598e5ff5014d0fd94a8a77c358e
rrt <rrt_gogs@liveintellect.org>
2019-03-16 12:42:23 -0600

    reverting grid points to Anycubic 9 x 9

--- Configuration.h
+++ Configuration.h
@@ -1088,7 +1088,7 @@
 #if ENABLED(AUTO_BED_LEVELING_LINEAR) || ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 7
+  #define GRID_MAX_POINTS_X 9
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
